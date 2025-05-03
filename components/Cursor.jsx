ce29fe75f6c9f6b94b39ca40b9de1ea047b42fcd
Jan <kupper.thorge@gmail.com>
2025-05-03 16:29:15 +0200

    custom cursor now only shows at 768px width or higher

--- components/Cursor.jsx
+++ components/Cursor.jsx
@@ -64,7 +64,7 @@ export default function Cursor() {
       style={{
         transform: `translate3d(${mouse.x}px, ${mouse.y}px, 0px)`,
       }}
-      className={`${isHoveringClickable ? "customCursor h-6 w-6 outline-offset-0" : "h-2 w-2 outline-offset-4"} pointer-events-none fixed top-0 left-0 z-[9999] hidden -translate-1/2 rounded-full bg-white mix-blend-difference outline outline-white transition-[width,height,background-color,outline-offset,outline-color] duration-200 sm:block`}
+      className={`${isHoveringClickable ? "customCursor h-6 w-6 outline-offset-0" : "h-2 w-2 outline-offset-4"} pointer-events-none fixed top-0 left-0 z-[9999] hidden -translate-1/2 rounded-full bg-white mix-blend-difference outline outline-white transition-[width,height,background-color,outline-offset,outline-color] duration-200 md:block`}
     />
   );
 }
