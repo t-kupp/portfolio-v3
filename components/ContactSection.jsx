7cde27c165bd1e95bda9233eae09702f533e07f2
Jan <kupper.thorge@gmail.com>
2025-08-18 11:18:00 +0200

    fixed text size bug

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -1,15 +1,14 @@
-import Divider from "./Divider";
-import SectionHeadline from "./SectionHeadline";
 import ContactForm from "./ContactForm";
 import Copyright from "./Copyright";
+import Divider from "./Divider";
+import SectionHeadline from "./SectionHeadline";
 
 export default function ContactSection() {
   return (
     <div className="bg-background relative flex flex-col px-4 pb-42 transition-colors duration-500 lg:px-8">
       <Divider />
       <SectionHeadline title={"Contact"} />
-
-      <h1 className="pb-12">Let's talk.</h1>
+      <h1 className="pb-12 text-[9rem] leading-none">Let's talk.</h1>
       <div className="mb-14 flex items-center gap-4">
         <a
           href="https://www.linkedin.com/in/jan-thorge-kupper/"
